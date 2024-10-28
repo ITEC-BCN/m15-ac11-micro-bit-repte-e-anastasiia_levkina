0fa69b5e312115516972bd3b1c0148b9ca9ed9c5
Anastasiia Lekvina <146827165+AnastasiiaLevkina@users.noreply.github.com>
2024-10-28 16:49:10 +0100

    Actualització pxt.json, main.ts, main.py

--- main.py
+++ main.py
@@ -1,1 +1,37 @@
+#constants
+max_temp = 50
+max_accel = 1023
+max_light = 255
+max_mg_field = 0
+max_rotation = 360
+max_sound = 255
 
+# variables
+current_temp = 0
+current_accel = 0
+current_light = 0
+mg_field = 0
+current_rotation = 0
+current_sound = 0
+
+modes = 6
+mode = 1
+
+def on_forever():
+    if mode == 0:
+        current_temp = input.temperature()
+        draw_temp_graph()
+    elif mode == 1:
+        current_accel = input.acceleration(Dimension.X)
+        draw_accel_graph()
+basic.forever(on_forever)
+
+def on_button_pressed_a():
+    mode = (mode + 1) % modes
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def draw_temp_graph():
+    led.plot_bar_graph(current_temp, max_temp, True)
+
+def draw_accel_graph():
+    led.plot_bar_graph(current_accel, max_accel, True)
--- main.ts
+++ main.ts
@@ -1,1 +1,39 @@
+// constants
+let max_temp = 50
+let max_accel = 1023
+let max_light = 255
+let max_mg_field = 0
+let max_rotation = 360
+let max_sound = 255
+//  variables
+let current_temp = 0
+let current_accel = 0
+let current_light = 0
+let mg_field = 0
+let current_rotation = 0
+let current_sound = 0
+let modes = 6
+let mode = 1
+basic.forever(function on_forever() {
+    let current_temp: number;
+    let current_accel: number;
+    if (mode == 0) {
+        current_temp = input.temperature()
+        draw_temp_graph()
+    } else if (mode == 1) {
+        current_accel = input.acceleration(Dimension.X)
+        draw_accel_graph()
+    }
+    
+})
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    let mode = (mode + 1) % modes
+})
+function draw_temp_graph() {
+    led.plotBarGraph(current_temp, max_temp, true)
+}
+
+function draw_accel_graph() {
+    led.plotBarGraph(current_accel, max_accel, true)
+}
 
